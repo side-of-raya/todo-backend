a48f6407abb97d24d4fbfea9b32c81bddb81825a
user <gitlinden@gmail.com>
2019-10-14 17:41:31 +0300

    Added ESlint

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({ origin: '*' }));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', require('./routes/todos'));
+app.use('/', require('./routes/users'));
+
+app.listen(3001);
