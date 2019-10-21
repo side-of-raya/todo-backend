9fca49f2ce1742912652b2a5ce1d431216b885cb
user <gitlinden@gmail.com>
2019-10-21 11:58:13 +0300

    http codes are set according to their meaning

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,14 +6,15 @@ const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
     const data = jwt.verify(token, process.env.KEY);
-    const user = await users.findOne({ id: data.id, token });
+    const user = await users.findOne({ id: data.id });
     if (!user) {
-      res.sendStatus(401);
+      res.sendStatus(403);
       return;
     }
     next();
   } catch (error) {
     console.log(error);
+    res.status(403).send("Invalid token.");
   }
 };
 
