4c6d1630ae94eca0d5f40bae684abf465eafa446
user <gitlinden@gmail.com>
2019-10-22 16:36:28 +0300

    New define structure

--- models/todos.js
+++ models/todos.js
@@ -1,23 +1,21 @@
-const db = require('../db/db');
-const { Sequelize, DataTypes } = require('../node_modules/sequelize');
-
-const todos = db.define('todos', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-  },
-  value: {
-    type: Sequelize.DataTypes.STRING,
-  },
-  is_checked: {
-    type: Sequelize.DataTypes.BOOLEAN,
-    default: false,
-  },
-});
-
-module.exports = todos;
+module.exports = (Sequelize, DataTypes) => {
+  const todos = Sequelize.define('todos', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+    },
+    value: {    
+      type: DataTypes.STRING,
+    },
+    is_checked: {
+      type: DataTypes.BOOLEAN,
+      default: false,
+    },
+  });
+  return todos;
+}
--- models/users.js
+++ models/users.js
@@ -1,65 +1,63 @@
 require('dotenv').config('../.env');
 const bcrypt = require('bcryptjs');
-const { Sequelize, DataTypes } = require('sequelize');
 const jwt = require('jsonwebtoken');
-const db = require('../db/db');
 
-const users = db.define('users', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  name: {
-    type: Sequelize.DataTypes.CHAR(20),
-  },
-  email: {
-    type: Sequelize.DataTypes.CHAR(30),
-  },
-  password: {
-    type: DataTypes.TEXT,
-  },
-});
+module.exports = (Sequelize, DataTypes) => {
+  const users = Sequelize.define('users', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.CHAR(20),
+    },
+    email: {
+      type: DataTypes.CHAR(30),
+    },
+    password: {
+      type: DataTypes.TEXT,
+    },
+  });
 
-users.signUp = async function(req, res) {
-  try {
-    const user = await users.findOne({
-      where: {
-        email: req.body.email
-      }
-    })
-    if (user) return res.sendStatus(403)
-    users.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: await bcrypt.hash(req.body.password, 8),
-    }).then(
-      res.sendStatus(200)
-    )
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(400);
-  }
-};
-
-users.generateAuthToken = function(user) {
-  const token = jwt.sign({ id: user.id }, process.env.KEY);
-  return token;
-};
-
-users.findByCredentials = async (email, password) => {
-  try {
-    const user = await users.findOne({ where: { email } });
-    const isPasswordMatch = await bcrypt.compare(password, user.password);
-    if (!user || !isPasswordMatch) {
-      res.status(403).send('wrong login or pass');
+  users.signUp = async function(req, res) {
+    try {
+      const user = await models.users.findOne({
+        where: {
+          email: req.body.email
+        }
+      })
+      if (user) return res.sendStatus(403)
+      await users.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hash(req.body.password, 8),
+      })
+      res.status(200).send(result)
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(400);
     }
-    return user;
-  } catch (error) {
-    console.log(error);
-    res.status(404).send('smth s went wrong')
-  }
-};
+  };
 
-module.exports = users;
+  users.generateAuthToken = function(user) {
+    const token = jwt.sign({ id: user.id }, process.env.KEY);
+    return token;
+  };
+
+  users.findByCredentials = async (email, password) => {
+    try {
+      const user = await users.findOne({ where: { email } });
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (!user || !isPasswordMatch) {
+        res.status(403).send('wrong login or pass');
+      }
+      return user;
+    } catch (error) {
+      console.log(error);
+      res.status(404).send('smth s went wrong')
+    }
+  };
+  return users;
+}
