389474b59234a99f0a55ef722f48e8383b41163a
user <gitlinden@gmail.com>
2019-10-22 16:34:23 +0300

    Partly added proccess conveyer

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ require('dotenv').config('../.env');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const db = require('./db');
 const app = express();
 
 app.use(cors({ origin: '*' }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+models = require('./db').models;
+
+app.set('models', db.models);
+app.set('db', db.db);
 
 app.use('/', require('./routes/todos'));
 app.use('/', require('./routes/users'));
