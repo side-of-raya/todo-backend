e5ba676e9cbf2a55729d753adaed6c70a8c0b2b6
user <gitlinden@gmail.com>
2019-10-28 11:49:13 +0300

    Changed error handling

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,17 +5,21 @@ const auth = async (req, res, next) => {
   try {
     const models = res.app.get('models');
     const token = req.headers.authorization;
+    if (!token) {
+      res.status(403).send("Invalid token");
+      return;
+    }
     const data = jwt.verify(token, process.env.KEY);
     const user = await models.users.findOne({ id: data.id });
     if (!user) {
-      res.sendStatus(403);
+      res.sendStatus(401);
       return;
     }
     res.locals.user = user;
     next();
   } catch (error) {
     console.log(error);
-    res.status(403).send("Invalid token.");
+    res.status(500).send("oops, smth s went wrong");
   }
 };
 
